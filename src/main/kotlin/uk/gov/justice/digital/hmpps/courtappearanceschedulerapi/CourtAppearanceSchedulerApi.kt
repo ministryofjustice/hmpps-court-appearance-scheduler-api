099e1d16e1420008284b35c61f13a145950d3ccd
runner <runner@hmpps-github-actions-runner-b9fb4594c-vsl85>
2026-03-23 16:25:07 +0000

    Rename project

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/CourtAppearanceSchedulerApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/CourtAppearanceSchedulerApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.courtappearanceschedulerapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class CourtAppearanceSchedulerApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<CourtAppearanceSchedulerApi>(*args)
 }
