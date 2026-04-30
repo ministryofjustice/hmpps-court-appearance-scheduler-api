8e6e0276a60808529a531e7a848616b96863864f
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2026-04-30 16:50:19 +0100

    MIA-1844 swagger docs (#30)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/controller/CourtAppearanceReasonController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/controller/CourtAppearanceReasonController.kt
@@ -1,13 +1,16 @@
 package uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.controller
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.access.Roles
+import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.config.OpenApiTags.UI
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.model.CourtAppearanceReasons
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.service.ReasonRetriever
 
+@Tag(name = UI)
 @RestController
 @RequestMapping("/court-appearance-reasons")
 @PreAuthorize("hasAnyRole('${Roles.SCHEDULER_RO}', '${Roles.SCHEDULER_RW}', '${Roles.SCHEDULER_UI}')")
