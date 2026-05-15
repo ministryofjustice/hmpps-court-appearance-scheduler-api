607ca40b3e5674e1506c7aaf0bc650e23344d562
Anthony Britton <anthony.britton@justice.gov.uk>
2026-05-15 16:30:05 +0100

    MIA-1882 inactive reasons

---
+++ src/main/resources/db/data/V0_5__inactive_reasons.sql
@@ -0,0 +1,5 @@
+insert into court_appearance_reason(code, description, external, sequence_number, active)
+values ('CAP', 'Court appearance', true, 510, false),
+       ('IM', 'Immigration hearing', true, 520, false),
+       ('SEN', 'Sentenced', true, 530, false)
+;
