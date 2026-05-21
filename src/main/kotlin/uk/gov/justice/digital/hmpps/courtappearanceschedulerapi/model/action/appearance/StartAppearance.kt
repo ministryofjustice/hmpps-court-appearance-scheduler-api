8e3163e43c5ca640f93f01586e43862d8347881f
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2026-05-21 11:05:26 +0100

    MIA-1902 completed without IN movement (#50)
    * Update all non major Gradle dependencies
    
    * MIA-1902 completed without IN movement
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/model/action/appearance/StartAppearance.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/model/action/appearance/StartAppearance.kt
@@ -1,17 +1,11 @@
 package uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.model.action.appearance
 
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.domain.CourtAppearance
-import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.domain.CourtAppearanceMovement.Direction.OUT
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.events.CourtAppearanceStarted
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.events.DomainEvent
 
 data class StartAppearance(
   override val reason: String? = null,
 ) : CourtAppearanceAction {
-  override fun domainEvent(ca: CourtAppearance): DomainEvent<*> = CourtAppearanceStarted(
-    ca.person.identifier,
-    checkNotNull(ca.latestMovement(OUT)).id,
-    ca.id,
-    ca.externalReference,
-  )
+  override fun domainEvent(ca: CourtAppearance): DomainEvent<*> = CourtAppearanceStarted(ca.person.identifier, ca.id, ca.externalReference)
 }
