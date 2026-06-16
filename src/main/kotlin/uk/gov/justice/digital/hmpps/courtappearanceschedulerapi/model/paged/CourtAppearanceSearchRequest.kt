4d248ea0f92ae7d9117ff5442d92d36b50c12ba9
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2026-06-16 10:09:49 +0100

    Mark lists as not required

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/model/paged/CourtAppearanceSearchRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/model/paged/CourtAppearanceSearchRequest.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.model.paged
 
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.domain.CourtAppearance
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.domain.CourtAppearanceStatus
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.model.ValidStartAndEnd
@@ -10,8 +12,11 @@ import java.time.LocalDate
 data class CourtAppearanceSearchRequest(
   override val start: LocalDate,
   override val end: LocalDate,
+  @Schema(requiredMode = NOT_REQUIRED)
   override val status: Set<CourtAppearanceStatus.Code> = emptySet(),
+  @Schema(requiredMode = NOT_REQUIRED)
   override val reason: Set<String> = emptySet(),
+  @Schema(requiredMode = NOT_REQUIRED)
   override val courtCodes: Set<String> = emptySet(),
   override val external: Boolean? = null,
   val query: String? = null,
