a272b13dee2ca4dace94ff8ecb8285f5dfbe4464
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2026-08-13 09:18:18 +0100

    MIA-2196 enable ras<->cas dps only in prod (#162)
    * MIA-2196 enable ras<->cas dps only in prod
    
    * Update dependency io.sentry:sentry-spring-boot-4 to v8.53.0
    
    * Update ministryofjustice/hmpps-github-shared-actions action to v1.1.0
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/config/ServiceConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/config/ServiceConfig.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.actuate.info.Info
 import org.springframework.boot.actuate.info.InfoContributor
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.events.domain.RasAppearanceInserted
+import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.events.domain.RasAppearanceUpdated
 import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.model.integration.IntegrationUrlBuilder
 import java.time.Duration
 
@@ -28,6 +30,8 @@ data class ServiceConfig(
 ) {
   data class DomainEventConfig(val pollInterval: Duration, val batchSize: Int, val disabledEvents: Set<String> = setOf())
   data class RasConfig(val enableWithResync: Boolean, val enableWithSync: Boolean, val sendUpdates: Boolean)
+
+  fun isProcessingRasEvents(): Boolean = setOf(RasAppearanceInserted.EVENT_TYPE, RasAppearanceUpdated.EVENT_TYPE).none { it in disablePrisonEvents }
 }
 
 @Component
