35ce8fc6a16566e2dc6760b85c1f7231d53af07a
Anthony Britton <anthony.britton@justice.gov.uk>
2026-08-25 16:13:14 +0100

    MIA-2140 remove responsible prison event

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/integration/prisonapi/PrisonerMovement.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/integration/prisonapi/PrisonerMovement.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.integration.prisonapi
 
-import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.integration.prisonapi.PrisonerMovement.Companion.DEFAULT_LOCATION
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -13,24 +12,11 @@ data class PrisonerMovement(
   val movementTime: LocalTime,
 ) {
   val movementDateTime: LocalDateTime = LocalDateTime.of(movementDate, movementTime)
-  fun responsibilityChangeMovement() = movementType in MOVEMENTS_OF_INTEREST && toAgency != null
-
-  fun responsiblePrisonCode(): String? = toAgency?.takeIf { movementType == ADMISSION_TYPE }
 
   companion object {
     const val ADMISSION_TYPE = "ADM"
     const val RELEASE_TYPE = "REL"
-    const val DEFAULT_LOCATION = "OUT"
-
-    private val MOVEMENTS_OF_INTEREST = setOf(ADMISSION_TYPE, RELEASE_TYPE)
   }
-}
-
-fun List<PrisonerMovement>.locationAt(at: LocalDateTime): String = movementBefore(at)?.responsiblePrisonCode() ?: DEFAULT_LOCATION
-
-fun List<PrisonerMovement>.movementBefore(before: LocalDateTime): PrisonerMovement? {
-  val ofInterest = filter { it.responsibilityChangeMovement() }.sortedByDescending { it.movementDateTime }
-  return ofInterest.firstOrNull { before.isAfter(it.movementDateTime) }
 }
 
 fun List<PrisonerMovement>.mostRecent(): PrisonerMovement? = maxByOrNull { it.movementDateTime }
