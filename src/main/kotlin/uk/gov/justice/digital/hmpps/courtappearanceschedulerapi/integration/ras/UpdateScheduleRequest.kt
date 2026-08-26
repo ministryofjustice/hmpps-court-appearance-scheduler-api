b91b714e470703a0e0c86df754b1273482ade164
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2026-08-26 17:12:46 +0100

    Revert to sending prison code for ras (#177)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/integration/ras/UpdateScheduleRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtappearanceschedulerapi/integration/ras/UpdateScheduleRequest.kt
@@ -4,10 +4,11 @@ import uk.gov.justice.digital.hmpps.courtappearanceschedulerapi.domain.appearanc
 import java.time.LocalDateTime
 
 data class UpdateScheduleRequest(
+  val prisonCode: String,
   val courtCode: String,
   val reasonCode: String,
   val start: LocalDateTime,
   val comments: String?,
 )
 
-fun CourtAppearance.asUpdateRequest() = UpdateScheduleRequest(courtCode, reason.code, start, comments)
+fun CourtAppearance.asUpdateRequest() = UpdateScheduleRequest(person.prisonCode!!, courtCode, reason.code, start, comments)
